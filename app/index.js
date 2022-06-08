90fcffb255758ecf5262902a2ab4869b82a83cb0
Viachaslau Abaimau <w.abaimov@gmail.com>
2022-06-08 13:04:10 +0300

    dev start

--- app/index.js
+++
@@ -1,5 +0,0 @@
-import ReactDOM from "react-dom/client";
-import React from "react";
-import App from "./app";
-
-ReactDOM.createRoot(document.getElementById("app")).render(<App />);
--- webpack.config.js
+++ webpack.config.js
@@ -1,46 +1,66 @@
+const webpack = require("webpack");
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+const production = process.env.NODE_ENV === 'production';
+
 
 module.exports = {
-  mode: "development",
-  entry: "./app/index.js", // входная точка - исходный файл
-  output: {
-    path: path.resolve(__dirname, "./public"), // путь к каталогу выходных файлов - папка public
-    publicPath: "/public/",
-    filename: "bundle.js", // название создаваемого файла
-  },
-  devServer: {
-    historyApiFallback: true,
-    static: {
-      directory: path.join(__dirname, "/"),
+    entry: { myAppName: path.resolve(__dirname, "./src/index.js") },
+    output: {
+        path: path.resolve(__dirname, "./dist"),
+        filename: production ? '[name].[contenthash].js' : '[name].js',
     },
-    port: 8081,
-    open: true,
-  },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: ["babel-loader"],
+            },
+            {
+                test: /\.s(a|c)ss$/,
+                exclude: /node_modules/,
+                use: [
+                    production ? MiniCssExtractPlugin.loader : 'style-loader',
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            modules: true,
+                            sourceMap: !production
+                        }
+                    },
+                    {
+                        loader: 'sass-loader',
+                        options: {
+                            sourceMap: !production
+                        }
+                    }
+                ]
+            },
 
-  module: {
-    rules: [
-      {
-        test: /\.jsx?$/, // определяем тип файлов
-        exclude: /(node_modules)/, // исключаем из обработки папку node_modules
-        loader: "babel-loader", // определяем загрузчик
-        options: {
-          presets: ["@babel/preset-react"], // используемые плагины
-        },
-      },
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /(node_modules|bower_components)/,
-        use: {
-          loader: "babel-loader",
-          options: {
-            presets: ["@babel/preset-react"],
-          },
-        },
-      },
-      {
-        test: /\.s[ac]ss$/i,
-        use: ["style-loader", "css-loader", "sass-loader"],
-      },
+        ],
+    },
+    resolve: {
+        extensions: ["*", ".js", ".jsx", ".scss"],
+    },
+    plugins: [
+        new CleanWebpackPlugin(),
+        new webpack.HotModuleReplacementPlugin(),
+        new HtmlWebpackPlugin({
+            title: "Webpack & React",
+            template: "./src/index.html",
+            favicon: "./public/favicon.ico"
+        }),
+        new MiniCssExtractPlugin({
+            filename: production ? '[name].[contenthash].css' : '[name].css',
+        }),
     ],
-  },
+    devServer: {
+        port: 3001,
+        hot: true,
+    },
+    mode: production ? 'production' : 'development'
 };
